5f499df7716cae6f86a64981e25f74b8de8d76a8
Chmouel Boudjnah <chmouel@chmouel.com>
2020-01-30 14:55:13 +0100

    I am funny!

--- new-pull-request.sh
+++ new-pull-request.sh
@@ -21,12 +21,25 @@ git checkout -b $branch master
 
 echo $word > ci
 
+set +e
+i=0
+while true;do
+    randomgif=$(curl -s -L 'https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=happy&rating=g&limit=100'|python -c "import json, random, sys;j = json.loads(sys.stdin.read());print(j['data'][random.randint(0,99)]['images']['fixed_height_downsampled']['url'])")
+    [[ -n ${randomgif} ]] && break
+    [[ $i == 9 ]] && {
+        randomgif="https://media2.giphy.com/media/12PIT4DOj6Tgek/200_d.gif?cid=e1bb72ff0d9684e5c8ec6e185c0c82366e11d9151d141280&rid=200_d.gif"
+        break
+    }
+    (( i+=1 ))
+    sleep 1
+done
+echo ${randomgif}
+set -e
+
 cat > ${TMP} <<EOF
-[TEST] OpenShift Pipeline CI: $word
+[TEST] OpenShift Pipeline CI
 
-We used to think that bird is the word but the best word in the world is
-$word
-
+![and now for something different](${randomgif})
 EOF
 
 
