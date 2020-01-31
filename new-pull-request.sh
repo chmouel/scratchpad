04134905f8bb6eda8eecd593874422b8a778657e
Chmouel Boudjnah <chmouel@chmouel.com>
2020-01-31 15:41:27 +0100

    NO random words anymore that would not be available in a job
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- new-pull-request.sh
+++ new-pull-request.sh
@@ -1,32 +1,23 @@
 #!/usr/bin/env bash
 set -eu
-
 TMP=$(mktemp /tmp/.mm.XXXXXX)
 clean() { rm -f ${TMP}; }
 trap clean EXIT
+dt=$(date "+%Y%m%d-%Hh%MS%S")
 
 git checkout master
 git branch -l|grep pull-branch|xargs -n1 git branch -D
 
-randomword () {
-	set -x
-	local total randomn
-	total=$(wc -l /usr/share/dict/words|cut -d " " -f1)
-	randomn=$(jot -r 1 1 ${total})
-	sed "${randomn}q;d" /usr/share/dict/words
-}
-
-word=$(randomword)
-branch=pull-branch-${word}
+branch=pull-branch-${dt}
 git checkout -b $branch master
 
-echo $word > ci
+echo $dt > CI
 
 GIF_KEYWORDS=work
 set +e
 i=0
 while true;do
-    randomgif=$(curl -s -L "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=${GIF_KEYWORDS}&rating=g&limit=100"|python -c "import json, random, sys;j = json.loads(sys.stdin.read());print(j['data'][random.randint(0,99)]['images']['fixed_height_downsampled']['url'])")
+    randomgif=$(curl -s -L "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=${GIF_KEYWORDS}&rating=g&limit=100"|python -c "import json, random, sys;j = json.loads(sys.stdin.read());print(j['data'][random.randint(0,len(j['data']))]['images']['fixed_height_downsampled']['url'])")
     [[ -n ${randomgif} ]] && break
     [[ $i == 9 ]] && {
         randomgif="https://i.giphy.com/media/hOzfvZynn9AK4/200_d.gif"
@@ -42,12 +33,11 @@ cat > ${TMP} <<EOF
 
 ![a funny gif, while you wait](${randomgif})
 
-Word of the day is ${word}
 EOF
 
 
-git add ci
-git commit -F ${TMP} ci
+git add CI
+git commit -F ${TMP} CI
 
 hub pull-request -F ${TMP} -p -b master -l CI
 
