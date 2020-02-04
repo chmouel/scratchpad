5cc416df2f17d6ea31ee78e480e1f47b5649f42d
Chmouel Boudjnah <chmouel@chmouel.com>
2020-02-04 12:50:42 +0100

    use ubi platform

--- new-pull-request.sh
+++ new-pull-request.sh
@@ -7,6 +7,8 @@ dt=$(date "+%Y%m%d-%Hh%MS%S")
 
 [[ -x ./close-all-pr.py ]] && ./close-all-pr.py
 
+python=python3
+[[ -x /usr/libexec/platform-python ]] && python=/usr/libexec/platform-python
 
 git checkout master
 git branch -l|grep pull-branch|xargs -n1 git branch -D
@@ -20,7 +22,7 @@ GIF_KEYWORDS=work
 set +e
 i=0
 while true;do
-    randomgif=$(curl -s -L "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=${GIF_KEYWORDS}&rating=g&limit=100"|python -c "import json, random, sys;j = json.loads(sys.stdin.read());print(j['data'][random.randint(0,len(j['data']))]['images']['fixed_height_downsampled']['url'])")
+    randomgif=$(curl -s -L "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=${GIF_KEYWORDS}&rating=g&limit=100"|${python} -c "import json, random, sys;j = json.loads(sys.stdin.read());print(j['data'][random.randint(0,len(j['data']))]['images']['fixed_height_downsampled']['url'])")
     [[ -n ${randomgif} ]] && break
     [[ $i == 9 ]] && {
         randomgif="https://i.giphy.com/media/hOzfvZynn9AK4/200_d.gif"
