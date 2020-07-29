798c13a1ab772fa785ad05dd1145115e3acdce9b
Chmouel Boudjnah <chmouel@chmouel.com>
2020-07-29 15:34:44 +0200

    Remove tkaac stuff out of here
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- tekton/test/t.sh
+++
@@ -1,2 +0,0 @@
-cd $(dirname $(readlink -f ${0}))
-curl -k -H 'content-type: application/json' -H 'User-Agent: GitHub-Hookshot/eab82ad' -H 'X-GitHub-Event: pull_request' -H 'X-GitHub-Delivery: c005e900-ccd3-11ea-895a-26e77176b05c' -X POST -d@propen.json	$(echo https://$(oc get route $(oc get service -o name|fzf -1|cut -d/ -f2) -o jsonpath='{.spec.host}'))
